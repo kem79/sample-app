3f2270add26a670d2067179d8fb04f207f7ec31e
kem79 <marechal_marc@yahoo.com>
2015-03-10 09:11:11 +0000

    test email with 2 dots

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -42,7 +42,7 @@ class UserTest < ActiveSupport::TestCase
   
   test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-                           foo@bar_baz.com foo@bar+baz.com]
+                           foo@bar_baz.com foo@bar+baz.com foo@bar..com]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
@@ -57,9 +57,10 @@ class UserTest < ActiveSupport::TestCase
   end
   
   test "email should be saved lower case" do
-    @user.email = @user.email.upcase
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
     @user.save
-    assert @user.email == @user.email.downcase, "email should be saved lowercase in db"
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
   
   test "password should have a minimum length" do
