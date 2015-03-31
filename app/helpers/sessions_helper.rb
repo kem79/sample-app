b8ea0e5ded810d28c1ab4495fd87724655f81059
kem79 <marechal_marc@yahoo.com>
2015-03-31 05:45:14 +0000

    remember user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,7 +7,16 @@ module SessionsHelper
   end
   
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies.signed[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+    
   end
   
   def logged_in?
@@ -17,6 +26,12 @@ module SessionsHelper
   def log_out
     session.delete(:user_id)
     @current_user = nil
+  end
+  
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent.signed[:remember_token] = user.remember_token
   end
   
 end
