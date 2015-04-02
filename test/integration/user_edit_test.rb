c3b064f99c7cdd64ab5bd78faae01fe01b811817
kem79 <marechal_marc@yahoo.com>
2015-04-02 07:22:17 +0000

    edit user info

---
+++ test/integration/user_edit_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class UserEditTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @user = users(:marc)
+  end
+  
+  test "unsuccessfull edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), user: {name: "",
+                                    email: "foo@invalid",
+                                    password: "foo",
+                                    password_confirmation: "baz" }
+    assert_template 'users/edit'
+  end
+  
+  test "successful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), user: { name: name,
+                                    email: email,
+                                    password: "",
+                                    password_confirmation: ""}
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal @user.name, name
+    assert_equal @user.email, email
+  end
+end
